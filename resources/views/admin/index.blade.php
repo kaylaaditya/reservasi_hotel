b20aae673773a3469179a9bd69faa5500a09c80e
kaylaaditya <kaylaaditya123@outlook.com>
2023-10-21 09:11:27 +0700

    menyelesaikan bagian sidebar dan form awal

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin',['$title'=>'User Admin'])
+
+@section('content-header')
+<h1 class="m-0"> <i class="fas fa-users"></i>User Admin</h1>
+@endsection
+
+@section('content')
+Content
+@endsection
