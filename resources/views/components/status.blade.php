0aa1e8507f601f7544138f48b8f336c9279f48c0
kaylaaditya <kaylaaditya123@outlook.com>
2023-11-08 13:18:14 +0700

    form admin

---
+++ resources/views/components/status.blade.php
@@ -0,0 +1,8 @@
+@if ( session('status')=='store')
+
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <strong>Berhasil Simpan!</strong> Data telah berhasil disimpan.
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+
+@endif
