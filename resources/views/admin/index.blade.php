b21e553c5bd9214d64e5fa40a55a8c8f3b44daa3
kaylaaditya <kaylaaditya123@outlook.com>
2023-11-13 07:19:19 +0700

    membuat search

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,8 +9,14 @@
 <x-status />
 <div class="card">
     <div class="card-header">
-        <x-btn-create :link="route('admin.create')" />
-        <x-search />
+        <div class="row">
+            <div class="col">
+                <x-btn-create :link="route('admin.create')" />
+            </div>
+            <div class="col">
+                <x-search />
+            </div>
+        </div>
     </div>
     <x-card-table>
         <thead>
